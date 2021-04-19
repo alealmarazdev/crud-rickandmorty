3fc5b429fdc552c3120a3433902c7013af52dc6f
Alejandra Almaraz <alejandra.almaraz23@gmail.com>
2021-04-18 22:40:22 -0500

    Create styles for Character Page

--- src/pages/CharactersPage.js
+++ src/pages/CharactersPage.js
@@ -1,14 +1,16 @@
 import { Row, Layout } from 'antd'
+
+import styles from './CharacterPage.module.css'
 const { Header, Content } = Layout;
 
 const CharacterPage = () => {
 
   return (
     <Layout>
-      <Header>
+      <Header className={styles.header}>
         <h1>Rick & Morty</h1>
       </Header>
-      <Content >
+      <Content className={styles.content}>
         <Row gutter={[16, 16]} justify="center" align="top">
 
         </Row>
