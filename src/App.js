08cbc5084b43769fd9b3508c842eca8664c06065
Alejandra Almaraz <alejandra.almaraz23@gmail.com>
2021-04-18 22:26:50 -0500

    Add Ant Design Dependency

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import store from './redux/createStore';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import 'antd/dist/antd.css';
 import "./App.css";
 
 function App() {
